12f06b2c9b8851bd4ee48b60db5bde7c33287741
gongyuejian <gongyuejian.000@mapgis.com>
2024-03-05 16:16:35 +0800

    解决项目link报错问题

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import './widgets'
+import * as settings from './widgets/settings'
+import * as widgets from './widgets/widgets'
 
-import './settings'
+const install = (Vue) => {
+  for (const name in settings) {
+    const com = settings[name]
+    const registerName = com.options ? com.options.name : com.name
+    if (registerName in Vue.options.components) {
+      console.log(`发现同名组件[${registerName}],已取消该组件的注册`)
+    } else {
+      Vue.use(com)
+    }
+  }
+
+  for (const name in widgets) {
+    const com = widgets[name]
+    const registerName = com.options ? com.options.name : com.name
+    if (registerName in Vue.options.components) {
+      console.log(`发现同名组件[${registerName}],已取消该组件的注册`)
+    } else {
+      Vue.use(com)
+    }
+  }
+}
+
+export default {
+  install,
+}
